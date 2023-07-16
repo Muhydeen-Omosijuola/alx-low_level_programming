08b34695a3349e426b4bfffc6a64bd6552255cb4
Muhydeen-Omosijuola <shoetanmuhydeen@gmail.com>
2023-07-16 09:47:54 -0700

    0-pwqositive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - Determines if a number is positive, negative or zero.
- *
+ * main - Determines if a number is positive or negative
+ * 
  * Return: Always 0 (Success)
  */
 int main(void)
